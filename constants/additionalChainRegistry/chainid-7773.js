ef081d5e6240cfb3688d76c86fee79c2e449da87
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2026-08-04 20:02:06 -0400

    Create chainid-7773.js
    Add Chain Info for Emberchain

---
+++ constants/additionalChainRegistry/chainid-7773.js
@@ -0,0 +1,29 @@
+{
+  "name": "Emberchain",
+  "chain": "EMBR",
+  "title": "Emberchain Mainnet",
+  "rpc": [
+    "https://emberchain.org/api/rpc",
+    "https://emberchain.duckdns.org/api/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "EMBR",
+    "symbol": "EMBR",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://emberchain.org",
+  "shortName": "embr",
+  "chainId": 7773,
+  "networkId": 7773,
+  "icon": "ember",
+  "explorers": [
+    {
+      "name": "Ember Ledger",
+      "url": "https://emberchain.org/ledger",
+      "icon": "ember",
+      "standard": "none"
+    }
+  ]
+}
